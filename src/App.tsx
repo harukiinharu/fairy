6242db0cedf39a1c6656f921e5c1c54ae357d66c
春木Haruki <harukiinharu@gmail.com>
2025-07-06 21:10:36 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+const App: React.FC = () => {
+  // handle double click to fullscreen
+  const handleDoubleClick = () => {
+    const video = document.querySelector('video')
+    if (video) {
+      video.requestFullscreen()
+    }
+  }
+
+  return (
+    <>
+      <video
+        src='/fairy.mp4'
+        autoPlay
+        loop
+        muted
+        className='fixed top-0 left-0 w-full h-full object-cover z-[-1]'
+        onDoubleClick={handleDoubleClick}
+        controlsList="nodownload nofullscreen noremoteplayback"
+        disablePictureInPicture
+      />
+    </>
+  )
+}
+
+export default App
